e476255d792649acdadd3022fb2f916649078778
Diego Acosta <diegoacosta211@gmail.com>
2021-05-16 21:45:23 -0300

    React Router, Class components, class properties plugins

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 // import Pet from './Pet';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import SearchParams from './SearchParams';
+import Details from './Details';
 
 const App = () => (
   <div>
-    <h1 id="heading" className="h1">Adopt Me!</h1>
-    <SearchParams />
+    <Router>
+      <header>
+        <Link to="/">
+          <h1 id="heading" className="h1">Adopt Me!</h1>
+        </Link>
+      </header>
+      <Switch>
+        <Route path="/details/:id">
+          <Details />
+        </Route>
+        <Route path="/">
+          <SearchParams />
+        </Route>
+      </Switch>
+    </Router>
   </div>
 );
 
---
+++ src/Details.js
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+class Detail extends Component {
+  state = { loading: true };
+
+  async componentDidMount() {
+    const { id } = this.props.match.params;
+    const res = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+    const data = await res.json();
+    this.setState({
+      loading: false,
+      ...data.pets[0]
+    });
+    console.log(this.state);
+  }
+
+  render() {
+    const { loading, animal, breed, city, state, description, name } = this.state;
+    if (loading) return <h2>Loading....</h2>;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${city} - ${state}`}</h2>
+          <button>{`Adopt ${name}!`}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Detail);
