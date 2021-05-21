14ed0d142479c75973b1c042955fbc9504216334
Diego Acosta <diegoacosta211@gmail.com>
2021-05-21 00:11:50 -0300

    adding TailwindCss

--- src/App.js
+++ src/App.js
@@ -11,10 +11,17 @@ const App = () => {
 
   return (
     <ThemeContext.Provider value={theme}>
-      <div>
+      <div
+        className="p-0 m-0"
+        style={{
+          backgroundImage: "url('http://pets-images.dev-apis.com/pets/wallpaperA.jpg')"
+        }}
+      >
         <Router>
-          <header>
-            <Link to="/">
+          <header
+            className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 to-red-500"
+          >
+            <Link to="/" className="text-6xl text-white hover:text-gray-200">
               <h1 id="heading" className="h1">Adopt Me!</h1>
             </Link>
           </header>
