4822cebaaec40f77c028024e722bc096b5ca75be
Diego Acosta <diegoacosta211@gmail.com>
2021-06-04 00:45:20 -0300

    fix

--- src/__tests__/useBreedList.test.js
+++ src/__tests__/useBreedList.test.js
@@ -1,23 +1,36 @@
 import { expect, test } from '@jest/globals';
-import { render } from '@testing-library/react';
 import { renderHook } from '@testing-library/react-hooks';
 import useBreedList from '../useBreedList';
 
-const getBreedList = animal => {
-  let list;
-
-  const TestComponent = () => {
-    list = useBreedList(animal);
-    return null
-  };
-
-  render(<TestComponent />);
-  return list;
-};
-
 test('Gives an empty list with no animal', async () => {
-  const [breedList, status] = getBreedList();
+  const { result } = renderHook(() => useBreedList());
+  const [breedList, status] = result.current;
 
   expect(breedList).toHaveLength(0);
   expect(status).toBe('unloaded');
-})
+});
+
+test('Gives back breeds with an animal', async () => {
+  const breeds = [
+    "Havanese",
+    "Bichon Frise",
+    "Poodle",
+    "Maltese",
+    "Golden Retriever",
+    "Labrador",
+    "Husky",
+  ];
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      animal: "dog",
+      breeds,
+    })
+  );
+  const { result, waitForNextUpdate } = renderHook(() => useBreedList("dog"));
+
+  await waitForNextUpdate();
+
+  const [breedList, status] = result.current;
+  expect(status).toBe("loaded");
+  expect(breedList).toEqual(breeds);
+});
